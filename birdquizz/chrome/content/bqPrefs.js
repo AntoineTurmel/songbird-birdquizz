5531ea33f6499717cc425031b0773d2181a2540b
Antoine Turmel <AntoineTurmel@users.noreply.github.com>
2008-06-11 16:20:50 +0000

    preferences !

---
+++ birdquizz/chrome/content/bqPrefs.js
@@ -0,0 +1,19 @@
+const choices	= "extensions.birdquizz.choices";
+const maxRounds	= "extensions.birdquizz.maxRounds";
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+
+var birdquizzPrefsPane = {
+	openPreferences: function() {
+        var windowMediator = Cc["@mozilla.org/appshell/window-mediator;1"].
+                            getService(Ci.nsIWindowMediator);
+        var window = windowMediator.getMostRecentWindow("Songbird:Main");
+
+        window.SBOpenPreferences("paneBQ");
+	},
+
+	handleLoad: function() {
+	}
+}
+
+
