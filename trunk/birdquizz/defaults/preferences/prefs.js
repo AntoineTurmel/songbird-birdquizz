1c3d99da426acf427844b067128b903249004238
mitchell.f.1.2 <mitchell.f.1.2@90019d8b-3f4f-0410-86f8-07d37d4f0363>
2008-06-12 00:45:49 +0000

    Cleanup.

--- trunk/birdquizz/defaults/preferences/prefs.js
+++ trunk/birdquizz/defaults/preferences/prefs.js
@@ -2,7 +2,6 @@
 pref("extensions.birdquizz.firstrun", true);
 pref("extensions.birdquizz.choices", "5");
 pref("extensions.birdquizz.maxRounds", "3");
-pref("extensions.birdquizz.timeout", "16000");
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 // pref("extensions.{fbf32071-b7fc-402b-b6fd-86e50abe1d20}.description", "chrome://birdquizz/locale/birdquizz.properties");
