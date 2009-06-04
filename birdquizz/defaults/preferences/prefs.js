4c72799d4a4633ba1983a951916ba32bccd165ae
Simone Fabiano <simone.fabiano@gmail.com>
2009-06-04 14:46:51 +0000

    add configuration options for the random game type

--- birdquizz/defaults/preferences/prefs.js
+++ birdquizz/defaults/preferences/prefs.js
@@ -4,6 +4,11 @@ pref("extensions.birdquizz.choices", "5");
 pref("extensions.birdquizz.maxRounds", "5");
 pref("extensions.birdquizz.playwith", "playtitle");
 pref("extensions.birdquizz.sound", true);
+pref("extensions.birdquizz.random_title", true);
+pref("extensions.birdquizz.random_artist", true);
+pref("extensions.birdquizz.random_album", true);
+pref("extensions.birdquizz.random_year", false);
+
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 // pref("extensions.{fbf32071-b7fc-402b-b6fd-86e50abe1d20}.description", "chrome://birdquizz/locale/birdquizz.properties");
